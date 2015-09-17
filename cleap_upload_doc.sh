545787fe879f2e0fdbcc6a0f4da226f97e417307
Cristobal Navarro <crinavar@dcc.uchile.cl>
2015-09-17 13:55:58 -0300

    fixed REAME

--- cleap_upload_doc.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# script to upload documentation
-echo "uploading documentation..."
-rsync -r $2 /usr/local/share/doc/cleap-$1/html/ crinavar@dichato.dcc.uchile.cl:public_www/doc/cleap/
-echo "done."
